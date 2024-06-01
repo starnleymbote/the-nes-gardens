4e94dd17f15cc092779cf56d01a8032e93dee288
starnleymbote@gmail.com <stanley.njoroge1@accesscfa.com>
2024-06-01 15:19:34 +0300

    Code clean up

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,16 +1,21 @@
-<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/plugin.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/tinymce/tinymce-custom.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/shortcodes.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAuSiPhoDaOJ7aqtJVtQhYhLzwwJ7rQlmA"></script>
-<script src="{{ asset('js/map-single.js') }}"></script>
-<script src="{{ asset('js/marker.js') }}"></script>
-<script src="{{ asset('js/infobox.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jqueryui.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/chart.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/chart-init.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/countto.js') }}"></script>
+ <!-- Javascript -->
+ <script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/jquery.min.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/swiper-bundle.min.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/carousel.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/plugin.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/jquery.nice-select.min.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/rangle-slider.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/countto.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/shortcodes.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/animation_heading.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/main.js') }}"></script>
+ <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAuSiPhoDaOJ7aqtJVtQhYhLzwwJ7rQlmA"></script>
+ <script src="{{ asset('/js/map.js') }}"></script>
+ <script src="{{ asset('/js/marker.js') }}"></script>
+ <script src="{{ asset('/js/infobox.min.js') }}"></script>
+
+
+ <script type="text/javascript" src="{{ asset('/js/chart.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/chart-init.js') }}"></script>
+ <script type="text/javascript" src="{{ asset('/js/jqueryui.min.js') }}"></script>
