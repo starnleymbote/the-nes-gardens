e1ccfd410c59bb3a9be31ed7f72b6963a5bfbfb6
starnleymbote@gmail.com <stanley.njoroge1@accesscfa.com>
2024-08-25 01:08:49 +0300

    Updated the colors

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -32,7 +32,6 @@
                                 <path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z" fill="white"/></svg>
                             </a>
                          </li>
-                         <li><a href="https://www.tiktok.com/@nesvilleke" target="_blank" class="box-icon w-40 social"><i class="icon icon-pinterest"></i></a></li>
                          <li><a href="#" class="box-icon w-40 social" target="_blank"><i class="icon icon-instagram"></i></a></li>
                          <li><a href="#" class="box-icon w-40 social" target="_blank"><i class="icon icon-youtube"></i></a></li>
                      </ul>
